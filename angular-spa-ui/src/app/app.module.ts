4af52e075977bbd93ca247eaffc89b8c5a05d055
marianatsapiv <mtsapiv@godaddy.com>
2020-03-13 22:33:29 +0200

    Add  notfound route to UI

--- angular-spa-ui/src/app/app.module.ts
+++ angular-spa-ui/src/app/app.module.ts
@@ -3,14 +3,64 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import {
+  MatButtonModule,
+  MatCardModule, MatDividerModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatOptionModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatSelectModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainComponent } from './components/main/main.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import {FormsModule} from '@angular/forms';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'employee/:id',      component: EmployeeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    ToolbarComponent,
+    MainComponent,
+    EditEmployeeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule,
+    MatListModule,
+    MatGridListModule,
+    MatIconModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
